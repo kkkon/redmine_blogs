aed12aa1c77939927a6b5dc16706d325f1d8b879
ichizok <gclient.gaap@gmail.com>
2011-11-18 17:10:52 +0900

    modified url format for atom/rss

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect 'projects/:project_id/blogs/:action', :controller => 'blogs'
+  map.connect 'projects/:project_id/blogs.:format', :controller => 'blogs', :action => 'index'
   map.connect 'projects/:project_id/blogs/:action/:id', :controller => 'blogs', :action => 'show'
 end
