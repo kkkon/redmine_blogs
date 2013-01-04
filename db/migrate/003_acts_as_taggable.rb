f41f82338d40a64df73b33574808726804946b56
kkkon <diverKon+github@gmail.com>
2013-01-04 23:44:51 +0900

    initialfix migrate
    conflict other acts_as_taggable's migrate

--- db/migrate/003_acts_as_taggable.rb
+++ db/migrate/003_acts_as_taggable.rb
@@ -1,22 +1,37 @@
 class ActsAsTaggable < ActiveRecord::Migration
   def self.up
-    create_table :tags do |t|
-      t.column :name, :string
+    unless table_exists? :tags
+      create_table :tags do |t|
+        t.column :name, :string
+      end
+    else
+      add_column :tags, :name, :string unless column_exists? :tags, :name
     end
 
-    create_table :taggings do |t|
-      t.column :tag_id, :integer
-      t.column :taggable_id, :integer
+    unless table_exists? :taggings
+      create_table :taggings do |t|
+        t.column :tag_id, :integer
+        t.column :taggable_id, :integer
+
+        # You should make sure that the column created is
+        # long enough to store the required class names.
+        t.column :taggable_type, :string
+
+        t.column :created_at, :datetime
+      end
+    else
+      add_column :taggings, :tag_id, :integer unless column_exists? :taggings, :tag_id
+      add_column :taggings, :taggable_id, :integer unless column_exists? :taggings, :taggable_id
 
       # You should make sure that the column created is
       # long enough to store the required class names.
-      t.column :taggable_type, :string
+      add_column :taggings, :taggable_type, :string unless column_exists? :taggings, :taggable_type
 
-      t.column :created_at, :datetime
+      add_column :taggings, :created_at, :datetime unless column_exists? :taggings, :created_at
     end
 
-    add_index :taggings, :tag_id
-    add_index :taggings, [:taggable_id, :taggable_type]
+    add_index :taggings, :tag_id unless index_exists? :taggings, :tag_id
+    add_index :taggings, [:taggable_id, :taggable_type] unless index_exists? :taggings, [:taggable_id, :taggable_type]
   end
 
   def self.down
--- db/migrate/004_add_tagger_and_context.rb
+++ db/migrate/004_add_tagger_and_context.rb
@@ -2,11 +2,11 @@ class AddTaggerAndContext < ActiveRecord::Migration
   require_dependency 'acts_as_taggable_on/tagging'
 
   def self.up
-    add_column :taggings, :context, :string
-    add_column :taggings, :tagger_id, :integer
-    add_column :taggings, :tagger_type, :string
+    add_column :taggings, :context, :string unless column_exists? :taggings, :context
+    add_column :taggings, :tagger_id, :integer unless column_exists? :taggings, :tagger_id
+    add_column :taggings, :tagger_type, :string unless column_exists? :taggings, :tagger_type
 
-    add_index :taggings, [:taggable_id, :taggable_type, :context]
+    add_index :taggings, [:taggable_id, :taggable_type, :context] unless index_exists? :taggings, [:taggable_id, :taggable_type, :context]
 
     ActsAsTaggableOn::Tagging.update_all("context = 'tags'", "context = NULL")
   end
